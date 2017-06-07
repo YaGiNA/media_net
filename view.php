663d1ce27e7efa5b1ca5bfcaea453ead5db511dd
YaGiNA <yutachihaya@gmail.com>
2017-06-07 10:45:41 +0900

    :bug:debug of view

--- view.php
+++ view.php
@@ -21,7 +21,7 @@
     <hr>
     <?php
       if(is_file("./log.csv")){
-        if(is_readable("./log.csv", "r")){
+        if(is_readable("./log.csv")){
           $fp = fopen("./log.csv", "r");
           flock($fp, LOCK_SH);
           $count = 1;
@@ -32,6 +32,7 @@
             if(count($content) == 3){
               echo "<p>".$count;
               echo ":<strong>名前: $content[0]</strong>  ";
+              echo "投稿日時:<time>$content[1]</time><br>$content[2]</p>\n"
               echo "<hr>\n";
               $count++;
             }
