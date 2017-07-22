6aab6a1916b433a6da7c5ae9069003ee825a75e5
YaGiNA <yutachihaya@gmail.com>
2017-07-22 17:16:04 +0900

    Rede:bug: of var name

--- enq_museum.php
+++ enq_museum.php
@@ -7,13 +7,13 @@ if(strlen($_POST["name"]) != 0){
   if (isset($_POST['visited']) && is_array($_POST['visited'])) {
     $visited_csv = implode(", ", $_POST["visited"]);
   } else {
-    $want_csv = "null";
+    $visited_csv = "null";
   }
 
   if (isset($_POST['want']) && is_array($_POST['want'])) {
-    $want = implode(", ", $_POST["want"]);
+    $want_csv = implode(", ", $_POST["want"]);
   } else {
-    $want = "null";
+    $want_csv = "null";
   }
 
   $fp = fopen("./answer_list.csv", "a+");
